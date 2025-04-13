ceaf3ea6c7aecd536dde4a99d371f03f68c6c755
Matheus <matheus.alvesbr21@gmail.com>
2025-04-13 15:33:19 -0300

    solve problem

--- src/app/teste/page.js
+++ src/app/teste/page.js
@@ -42,7 +42,6 @@ export default function Teste({Titulo, TituloDesc, TituloIntroduc, TituloNum, li
                             )
                         })}
                     </ul>
-                    
                     {listasAlimentos?.map((item, index) =>{
                         return(
                             <Lists key={index} descriptionName={item.descriptionName} comoConsumir={item.comoConsumir} imageList={item.imageList} listName={item.listName} beneficios={item.beneficios}/>
