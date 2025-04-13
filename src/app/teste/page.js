83d0f165ffc4945b2b7dedfd756375a668d03599
Matheus <matheus.alvesbr21@gmail.com>
2025-04-13 15:47:00 -0300

    atualizado

--- src/app/teste/page.js
+++ src/app/teste/page.js
@@ -7,17 +7,18 @@ import MyFooter from "../layouts/footer";
 import MyLi from "../components/liDec";
 export default function Teste({Titulo, TituloDesc, TituloIntroduc, TituloNum, listas, ListAlimentos, capa }){
 
-    const [minhasListas,  definirMinhasListas] = useState([]);
-    const [listasAlimentos, definirListasAlimentos] = useState([]);
+
+    const [minhasListas,  definirMinhasListas] = useState(listas || []);
+    const [listasAlimentos, definirListasAlimentos] = useState(ListAlimentos|| []);
 
     useEffect(() =>{
-        if(listas){
-            definirMinhasListas(listas);
-        }
-        if(ListAlimentos){
-            definirListasAlimentos(ListAlimentos);
-        }
-    },[]);
+        
+            definirMinhasListas(listas || []);
+        
+        
+            definirListasAlimentos(ListAlimentos || []);
+        
+    },[listas, ListAlimentos]);
 
 
     return(
@@ -36,17 +37,17 @@ export default function Teste({Titulo, TituloDesc, TituloIntroduc, TituloNum, li
                     <h1 className="text-base sm:text-base md:text-base xl:text-2xl font-bold Poppins mt-6">{TituloNum}</h1>
                     <p className="text-xs sm:text-sm lg:text-base xl:text-lg font-semibold mt-2 text-neutral-700 dosis">{TituloIntroduc}</p>
                     <ul className="sm:mt-4 xl:pl-2 xl:pb-10">
-                        {Array.isArray(minhasListas) ? minhasListas.map((item, index) =>{
+                        { minhasListas.map((item, index) =>{
                             return(
                                 <MyLi key={index} name={item.name} number={item.number}/>
                             )
-                        }): ""}
+                        })}
                     </ul>
-                    {Array.isArray(listasAlimentos) ? listasAlimentos?.map((item, index) =>{
+                    {listasAlimentos?.map((item, index) =>{
                         return(
                             <Lists key={index} descriptionName={item.descriptionName} comoConsumir={item.comoConsumir} imageList={item.imageList} listName={item.listName} beneficios={item.beneficios}/>
                         )
-                    }) : ""}
+                    })}
                 </div>
                 
                 
